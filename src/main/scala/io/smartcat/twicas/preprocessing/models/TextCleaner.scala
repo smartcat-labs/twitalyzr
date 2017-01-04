5180a0618a412ab95361a8bd8b9fa0b31e5e0d2e
= <stankokuveljic@gmail.com>
2017-01-04 12:53:20 +0100

    Add tweet text cleaner with nasty regexp

---
+++ src/main/scala/io/smartcat/twicas/preprocessing/models/TextCleaner.scala
@@ -0,0 +1,20 @@
+package io.smartcat.twicas.preprocessing.models
+
+import io.smartcat.twicas.preprocessing.pipeline.Pipeline
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+class TextCleaner(columns: List[String]) extends Pipeline {
+  // ONE NASTY REGEXP
+  override def process(df: DataFrame): DataFrame = {
+    def cleanOne(df: DataFrame, columnName: String): DataFrame = {
+      df.withColumn(columnName,
+        trim(lower(
+          regexp_replace(
+            regexp_replace(df(columnName), """\/|\\|~|&|\^|\?|\!|\$|\(|\)|\\b\\w{1}\\b\\s?|\"|\'|RT|:|\.|@[^\s]+|http[^\s]+|#|[0-9]+|([^\x00-\x7F])""", ""),
+            """(\+|_|-|\,|\s+|;)""", " "))))
+    }
+
+    columns.foldLeft(df)((dff, columnName) => cleanOne(dff, columnName))
+  }
+}
