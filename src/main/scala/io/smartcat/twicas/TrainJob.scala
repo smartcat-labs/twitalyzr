8c5b470df7a55c5ed2a2c123cc812c3c536287e7
= <stankokuveljic@gmail.com>
2017-01-04 13:00:53 +0100

    Preprocessing showcase

--- src/main/scala/io/smartcat/twicas/TrainJob.scala
+++ src/main/scala/io/smartcat/twicas/TrainJob.scala
@@ -1,5 +1,7 @@
 package io.smartcat.twicas
 
+import io.smartcat.twicas.preprocessing.models._
+import io.smartcat.twicas.preprocessing.pipeline.PipelineProcessor
 import io.smartcat.twicas.tweet.Tweet
 import org.apache.spark.sql.SparkSession
 
@@ -8,7 +10,8 @@ object TrainJob extends App {
     .appName("twitter_trainer")
     .getOrCreate()
 
-  val filename = ""
+  //This is just for testing, whole dataset will be added later in project
+  val filename = "/home/stanko/Documents/dataset_parts/02_11_2016/raw_labeled.json"
 
   val jsonDF = spark.read.json(filename)
 
@@ -18,6 +21,16 @@ object TrainJob extends App {
 
   val df = rdd.toDF
 
-  df.show
+  val pipelineProcesor = new PipelineProcessor()
+  val textCleaner = new TextCleaner(List("text", "userDescription"))
+  val tokenizer = FeatureTokenizer.make(List("text", "userDescription"))
+  val hashingTF = FeatureHashTF.make(Map("text_t" -> 100, "userDescription_t" -> 100))
+  val result = pipelineProcesor.processAll(df, List(textCleaner, tokenizer, hashingTF))
+  result.show
+
+  val idf = FeatureIDF.make(result, List("text_t_tf", "userDescription_t_tf"))
+
+  val preprocessPipeline = pipelineProcesor.processAll(df, List(textCleaner, tokenizer, hashingTF, idf))
+  preprocessPipeline.show
 
 }
