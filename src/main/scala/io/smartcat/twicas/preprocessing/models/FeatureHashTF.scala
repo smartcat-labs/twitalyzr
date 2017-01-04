062fe313ea1512d1b3ae799090ea3f457ce01581
= <stankokuveljic@gmail.com>
2017-01-04 12:57:09 +0100

    Add hash term freq model

---
+++ src/main/scala/io/smartcat/twicas/preprocessing/models/FeatureHashTF.scala
@@ -0,0 +1,23 @@
+package io.smartcat.twicas.preprocessing.models
+
+import io.smartcat.twicas.preprocessing.pipeline.Pipeline
+import org.apache.spark.ml.feature.HashingTF
+import org.apache.spark.sql.DataFrame
+
+class FeatureHashTF(hashingModels: List[HashingTF]) extends Pipeline {
+  override def process(df: DataFrame): DataFrame = hashingModels.foldLeft(df)((dff, model) => model.transform(dff))
+}
+
+object FeatureHashTF extends Serializable {
+  val afterHashing = "_tf"
+
+  def make(columnNamesFeatureNum: Map[String, Int]): FeatureHashTF = {
+    new FeatureHashTF(columnNamesFeatureNum.keySet.map(column => {
+      new HashingTF()
+        .setInputCol(column)
+        .setOutputCol(column + afterHashing)
+        .setNumFeatures(columnNamesFeatureNum(column))
+    }).toList)
+  }
+
+}
