09effcd7c7e516317a00a89f2f343884c90c1e8f
= <stankokuveljic@gmail.com>
2017-01-11 12:09:46 +0100

    Add twitter stream

--- src/main/scala/io/smartcat/twicas/ClassifyJob.scala
+++ src/main/scala/io/smartcat/twicas/ClassifyJob.scala
@@ -1,5 +1,40 @@
 package io.smartcat.twicas
 
-object ClassifyJob extends App {
+import io.smartcat.twicas.tweet.Tweet
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.twitter.TwitterUtils
 
+object ClassifyJob extends App {
+  val spark = SparkSession.builder()
+    .appName("twitter_classifier")
+    .getOrCreate()
+
+  val token = ""
+  val tokenSecret = ""
+  val consumerKey = ""
+  val consumerSecret = ""
+
+  val searchFilter = "#cassandra"
+
+  val interval = 10
+
+  System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+  System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+  System.setProperty("twitter4j.oauth.accessToken", token)
+  System.setProperty("twitter4j.oauth.accessTokenSecret", tokenSecret)
+
+  val ssc = new StreamingContext(spark.sparkContext,Seconds(interval))
+  val tweetStream = TwitterUtils.createStream(ssc, None, Seq(searchFilter))
+
+  import spark.sqlContext.implicits._
+
+  tweetStream.foreachRDD((rdd, time) => {
+    val tweets = rdd.map(Tweet.makeStream)
+    val tweetsDF = tweets.toDF
+    tweetsDF.show(3)
+  })
+
+  ssc.start()
+  ssc.awaitTermination()
 }
