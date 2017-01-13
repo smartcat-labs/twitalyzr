c0393c2ddc3978368eb0f111bdf333f097167338
= <stankokuveljic@gmail.com>
2017-01-13 09:05:54 +0100

    Split Pipeline in two scala files

--- src/main/scala/io/smartcat/twicas/pipeline/PipelineProcessor.scala
+++ src/main/scala/io/smartcat/twicas/pipeline/PipelineProcessor.scala
@@ -2,14 +2,18 @@ package io.smartcat.twicas.pipeline
 
 import org.apache.spark.sql.DataFrame
 
-trait Pipeline extends Serializable {
-  def process(df: DataFrame): DataFrame
-}
-
 
 class PipelineProcessor(pipelines: List[Pipeline]) extends Serializable {
 
   def processAll(df: DataFrame): DataFrame = pipelines.foldLeft(df)((dff, pipeline) => pipeline.process(dff))
+
+}
+
+object PipelineProcessor extends Serializable{
+
+  def loadFromFile(filepath:String):PipelineProcessor = ???
+
+  def saveToFile(filepath:String, pipelineProcessor: PipelineProcessor):Unit = ???
 
 }
 
