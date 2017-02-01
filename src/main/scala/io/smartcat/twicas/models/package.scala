24a58808ae8bcf046a46dc92658d32dcb1f07ba1
= <stankokuveljic@gmail.com>
2017-02-01 10:59:17 +0100

    Change type of function

--- src/main/scala/io/smartcat/twicas/models/package.scala
+++ src/main/scala/io/smartcat/twicas/models/package.scala
@@ -2,7 +2,7 @@ package io.smartcat.twicas
 
 package object models {
 
-  def generator(x: List[List[Double]]): List[List[Double]] = x match {
+  def generator[T](x: List[List[T]]): List[List[T]] = x match {
     case Nil => List(Nil)
     case h :: _ => h.flatMap(i => generator(x.tail).map(i :: _))
   }
