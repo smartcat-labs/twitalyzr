f49f35667552fbeba62ce7febf339d3341364b60
= <stankokuveljic@gmail.com>
2017-02-02 19:26:06 +0100

    Add FeatureCountVectorizer - resolves issue #8

---
+++ src/main/scala/io/smartcat/twicas/preprocessing/FeatureCountVectorizer.scala
@@ -0,0 +1,34 @@
+package io.smartcat.twicas.preprocessing
+
+import io.smartcat.twicas.pipeline.Pipeline
+import io.smartcat.twicas.util.Conf
+import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel}
+import org.apache.spark.sql.DataFrame
+
+class FeatureCountVectorizer(countVectorizerModel: List[CountVectorizerModel]) extends Pipeline {
+  override def process(df: DataFrame): DataFrame = countVectorizerModel.foldLeft(df)((dff, model) => model.transform(dff))
+}
+
+object FeatureCountVectorizer extends Serializable {
+
+  val outputSufix: String = Conf.Preprocessing.Sufix.afterCountVectorizer
+  val minDF: Int = Conf.Preprocessing.CountVectorizer.minDocumentFrequency
+  val minTF: Int = Conf.Preprocessing.CountVectorizer.minTermFrequency
+
+  def make(df: DataFrame, vectorCounts: Map[String, Int]): FeatureCountVectorizer = {
+
+    val model = vectorCounts.map { case (k, v) =>
+      new CountVectorizer()
+        .setInputCol(k)
+        .setOutputCol(k + outputSufix)
+        .setBinary(false)
+        .setMinDF(minDF)
+        .setMinTF(minTF)
+        .setVocabSize(v)
+        .fit(df)
+    }
+
+    new FeatureCountVectorizer(model.toList)
+
+  }
+
