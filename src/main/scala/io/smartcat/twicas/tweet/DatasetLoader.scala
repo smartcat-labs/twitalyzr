b600f9631f111416d400c4642999733437685c0f
= <stankokuveljic@gmail.com>
2017-02-09 10:27:17 +0100

    Change class to object

--- src/main/scala/io/smartcat/twicas/tweet/DatasetLoader.scala
+++ src/main/scala/io/smartcat/twicas/tweet/DatasetLoader.scala
@@ -4,12 +4,12 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scala.io.Source
 
-class DatasetLoader {
+object DatasetLoader {
 
   def load(filename: String, spark: SparkSession): DataFrame = {
     import spark.sqlContext.implicits._
 
-    val resource = this.getClass.getResourceAsStream(filename)
+    val resource = DatasetLoader.getClass.getResourceAsStream(filename)
     val lines = Source.fromInputStream(resource).mkString
     val rddJson = spark.sparkContext.parallelize(Seq(lines))
     val dfJson = spark.sqlContext.read.json(rddJson)
